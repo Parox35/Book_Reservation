f4e3a90b607072dd07d2ce6e34b3c65f1996b96e
Parox35 <antoine.esnault4@gmail.com>
2024-05-23 15:17:56 +0200

    Add moving login to sign up

--- JS/login.js
+++ JS/login.js
@@ -1,4 +1,9 @@
 function signFunction(){
-    document.getElementById("div_sign").classList.remove("collapse");
+    document.getElementById("div_register").classList.remove("collapse");
     document.getElementById("div_login").classList.add("collapse");
+}
+
+function loginFunction(){
+    document.getElementById("div_").classList.remove("collapse");
+    document.getElementById("div_register").classList.add("collapse");
 }

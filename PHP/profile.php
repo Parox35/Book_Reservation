54e2c2e72b0b05aebc168f35a64c3a2fa2352cd1
Parox35 <antoine.esnault4@gmail.com>
2024-05-27 22:00:27 +0200

    Add profile

---
+++ PHP/profile.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=nav, initial-scale=1.0">
+    <title>Profile</title>
+
+    <link rel="stylesheet" href="../CSS/style.css">
+    <!--Boostrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
+    <!--Font awesome-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css">
+</head>
+<body class="d-flex flex-column min-vh-100">
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="./home.php"><i class="fas fa-book"></i></a>
+        </div>
+        <div class="container-fluid">
+            <a class="navbar-brand collapse" href="#"><i class="fas fa-book">Add book</i></a>
+        </div>
+        <a class="navbar-brand text-end" href="./profile.php"><i class="fas fa-user"></i></a>
+    </nav>
+    
+    <main>
+        
+    </main>
+    
+    <footer class="bg-dark mt-auto">
+        <p class="text-center text-secondary p-3 m-0">© 2024 Company, Inc</p>
+    </footer>
+</body>
+</html>

61f311cc402b558fd2e4659f5f7ecf2ce2e398ba
Parox35 <antoine.esnault4@gmail.com>
2024-05-28 22:30:25 +0200

    Add login function

--- JS/login.js
+++ JS/login.js
@@ -1,9 +1,13 @@
 function signFunction(){
-    document.getElementById("div_register").classList.remove("collapse");
-    document.getElementById("div_login").classList.add("collapse");
+    if(document.getElementById("div_register").classList.contains("collapse")){
+        document.getElementById("div_register").classList.remove("collapse");
+        document.getElementById("div_login").classList.add("collapse");
+    }
 }
 
 function loginFunction(){
-    document.getElementById("div_").classList.remove("collapse");
-    document.getElementById("div_register").classList.add("collapse");
+    if(document.getElementById("div_login").classList.contains("collapse")){
+        document.getElementById("div_login").classList.remove("collapse");
+        document.getElementById("div_register").classList.add("collapse");
+    }
 }
--- index.php
+++ index.php
@@ -1,13 +1,83 @@
-<?php
+<?php 
+session_start();
+  if (isset($_SESSION['email'])) {
+    header("Location: ./PHP/home.php");
+  }
+  if($_SERVER["REQUEST_METHOD"] == "POST"){
+    require_once("./PHP/config.php");
+  
+    //Register/Sign up
+    if(isset($_POST["sign_up"])){
+      $email = $_POST['email'];
+      $firstName = $_POST['firstName'];
+      $lastName = $_POST['lastName'];
+      $password = $_POST['password'];
 
+      $email = stripcslashes($email);
+      $password = stripcslashes($password);
+      $firstName = stripcslashes($firstName);
+      $lastName = stripcslashes($lastName);
 
+      $email = mysqli_real_escape_string($conn, $email);
+      $password = mysqli_real_escape_string($conn, $password);
+      $firstName = mysqli_real_escape_string($conn, $firstName);
+      $lastName = mysqli_real_escape_string($conn, $lastName);
+
+      $password = password_hash(mysqli_real_escape_string($conn, $_POST['password']), PASSWORD_BCRYPT);
+
+      $sql = "INSERT INTO users VALUES('{$email}','{$firstName}','{$lastName}','{$password}',0)";
+      $addUser = mysqli_query($conn, $sql);
+
+      if (!$addUser) {
+        echo "Something went wrong" . mysqli_error($conn);
+      } else {
+        //popup
+        header('location: index.php');
+      }
+    }
+
+    //Login 
+    if (isset($_POST['login'])) {
+      $email = $_POST["email"];
+      $password = $_POST["password"];
+
+      $email = stripcslashes($email);
+      $password = stripcslashes($password);
+      $email = mysqli_real_escape_string($conn, $email);
+      $password = mysqli_real_escape_string($conn, $password);
+      
+      $sql = "SELECT * from users WHERE Email = '$email' AND Password = '$password'";
+      $user = mysqli_query($conn, $sql);
+
+      if (!$user) {
+        die('query Failed' . mysqli_error($conn));
+      }
+      $user_email = "";
+      $user_password = "";
+      if(mysqli_num_rows($user) > 0){
+        while ($row = mysqli_fetch_array($user)) {
+
+          $user_email = $row['Email'];
+          $user_password = $row['Password'];
+        }
+      }
+      if ($user_email == $email  &&  $user_password == $password) {
+
+        $_SESSION['email'] = $user_email;       // Storing the value in session
+        header('location: ./PHP/home.php');
+        exit;
+      } else {
+        header('location: index.php');
+      }
+    }
+  }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=nav, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login page</title>
     <link href="./CSS/style.css" rel="stylesheet">
     <!--Boostrap-->
@@ -31,16 +101,16 @@
 
                   <div id="div_login">
                     <h3 class="text-center">Login</h3>
-                    <form action="." method="POST" class="text-center">
+                    <form action="" method="POST" class="text-center">
                       <div class="row">
-                        <label for="">Email:</label>
-                        <input type="email" class="col-sm-6 mx-auto">
+                        <label for="email">Email:</label>
+                        <input type="email" name="email" class="col-sm-6 mx-auto" required>
                       </div>
                       <div class="row">
-                        <label for="">Password:</label>
-                        <input type="password" class="col-sm-6 mx-auto">
+                        <label for="password">Password:</label>
+                        <input type="password" name="password" class="col-sm-6 mx-auto"  autocomplete="off" required>
                       </div>
-                      <input type="submit" value="Login" class="m-3 btn btn-outline-success">
+                      <input type="submit" value="Login" name="login" class="m-3 btn btn-outline-success">
                     </form>
                   </div>
                     
@@ -50,26 +120,22 @@
                     <h3 class="text-center">Sign up</h3>
                     <form action="" class="text-center">
                     <div class="row">
-                      <label for="">Email:</label>
-                      <input type="email" class="col-sm-6 mx-auto">
+                      <label for="email">Email:</label>
+                      <input type="email" name="email" class="col-sm-6 mx-auto" required>
                     </div>
                     <div class="row">
-                      <label for="">First Name:</label>
-                      <input type="text" class="col-sm-6 mx-auto">
+                      <label for="firstName">First Name:</label>
+                      <input type="text" name="firstName" class="col-sm-6 mx-auto" required>
                     </div>
                     <div class="row">
-                      <label for="">Last Name:</label>
-                      <input type="text" class="col-sm-6 mx-auto">
+                      <label for="lastName">Last Name:</label>
+                      <input type="text" name="lastName" class="col-sm-6 mx-auto" required>
                     </div>
                     <div class="row">
-                      <label for="">Age:</label>
-                      <input type="number" class="col-sm-6 mx-auto">
+                      <label for="password">Password:</label>
+                      <input type="password" name="password" class="col-sm-6 mx-auto" autocomplete="off" required>
                     </div>
-                    <div class="row">
-                      <label for="">Password:</label>
-                      <input type="password" class="col-sm-6 mx-auto">
-                    </div>
-                      <input type="submit" value="Sign up" class="m-3 btn btn-outline-success">
+                      <input type="submit" value="Sign up" name="sign_up" class="m-3 btn btn-outline-success">
                     </form>
                   </div>
 

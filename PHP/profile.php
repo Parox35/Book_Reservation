6c96ff23e4e8aa79989f8574dc1ccf34b1fc378e
Parox35 <antoine.esnault4@gmail.com>
2024-05-29 14:03:27 +0200

    Modify meta

--- PHP/profile.php
+++ PHP/profile.php
@@ -26,7 +26,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=nav, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile</title>
 
     <link rel="stylesheet" href="../CSS/style.css">

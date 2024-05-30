047a3f2317b491bc3b217ae02b441123fca9385c
Parox35 <antoine.esnault4@gmail.com>
2024-05-30 15:58:03 +0200

    Add comments to manage js

--- JS/manage.js
+++ JS/manage.js
@@ -1,3 +1,4 @@
+//Function moving to reservation view to administrator view
 function adminView(){
     document.getElementById("adminTable").classList.remove("collapse");
     document.getElementById("reservationTable").classList.add("collapse");
@@ -5,6 +6,7 @@ function adminView(){
     document.getElementById("btnReservation").disabled = false;
 }
 
+//Function moving to administrator view to reservation view
 function reservationView(){
     document.getElementById("reservationTable").classList.remove("collapse");
     document.getElementById("adminTable").classList.add("collapse");
@@ -12,11 +14,11 @@ function reservationView(){
     document.getElementById("btnAdmin").disabled = false;
 }
 
-
+//Get the id of the reservation
 var deleteModal = document.getElementById('deleteReservationModal');
 deleteModal.addEventListener('show.bs.modal', function (event) {
     var button = event.relatedTarget; // Button that triggered the modal
-    var id = button.getAttribute('data-id'); // Extract info from data-* attributes
+    var id = button.getAttribute('data-id'); // Extract info from data-id attributes
     var modalBodyInput = deleteModal.querySelector('#id'); // Find the hidden input field in the modal
     modalBodyInput.value = id; // Set the value of the hidden input field
 });
